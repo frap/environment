4054e133c7211185a34864981b41fbc98b39674b
Gas <gas@tuatara.red>
2022-06-02 13:59:34 +1200

    some git changes

--- eru-new.sh
+++ eru-new.sh
@@ -20,7 +20,7 @@ if [[ "$1" == "-h" || "$1" == "--help" ]]; then
 Usage: $(basename "$0")
 
 Used to install my setup on new VM from scratch.
-https://github.com/frap/environment
+https://github.com/frap/dotfiles.git
 
 Copyright (c) 2020 "Gas" Andrés Gasson
 Licensed under the MIT license.xs
@@ -40,41 +40,69 @@ function silence {
 #
 # Fetching the notes
 #
-
+export DEV=$HOME/Dev
+export DOTFILES=$DEV/dotfiles.git
 export XDG_CONFIG_HOME=$HOME/.config
+export CONFIG_BACKUP=$HOME/config-backup
 
-env_https=https://github.com/frap/environment
-env_ssh=git@github.com:frap/environment.git
+function gitdf {
+   git --git-dir=$DOTFILES --work-tree=$HOME $@
+}
+env_https=https://github.com/frap/dotfiles.git
+env_ssh=git@github.com:frap/dotfiles.git
 env_emacs=git@github.com:frap/emacs.git
 
-if [ -d "$XDG_CONFIG_HOME" ] && [ ! -d "$XDG_CONFIG_HOME/.git" ]; then
-	cd "$XDG_CONFIG_HOME" && {
-		git init
-		# clone via HTTPS, as most likely SSH is not yet available or configured
-		git remote add origin $env_https
-		git fetch
-		git reset --hard origin/main
-	}
+if [ ! -d "$DEV" ]; then
+    mkdir -p $DEV
 fi
 
-if [ ! -d "$XDG_CONFIG_HOME/.git" ]; then
+if [ ! -d "$DOTFILES/" ]; then
+    echo "Clonage du dépôt dotfiles.git vers $DOTFILES"
 	# clone via HTTPS, as most likely SSH is not yet available or configured
-	git clone $env_https "$XDG_CONFIG_HOME"
+	git clone --bare $env_https "$DEV"
+fi
+# move .config DIR if exists
+if [ -d "$XDG_CONFIG_HOME" ]; then
+    echo "Déplacement du répertoire .config vers config-backup"
+    mv "$XDG_CONFIG_HOME"  "$CONFIG_BACKUP"
+    #&& {
+# 		git init
+# 		# clone via HTTPS, as most likely SSH is not yet available or configured
+# 		git remote add origin $env_https
+# 		git fetch
+# 		git reset --hard origin/main
+# 	}
 fi
 
-cd "$XDG_CONFIG_HOME" && {
-	git remote set-url origin $env_ssh
-}
 
-# install emacs
-if [ ! -d "$XDG_CONFIG_HOME/emacs/.git" ]; then
-	# clone via HTTPS, as most likely SSH is not yet available or configured
-	git clone $env_emacs "$XDG_CONFIG_HOME"
+if [ -d "$DOTFILES/" ]; then
+    gitdf checkout
+    if [ $? = 0 ]; then
+      echo "Dotfiles vérifiée.";
+    else
+        if [ ! d "$CONFIG_BACKUP" ]; then
+            mkdir -p "$CONFIG_BACKUP"
+        fi
+        echo "Sauvegarde préexistante dotfiles.";
+        gitdf checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} $CONFIG_BACKUP/{}
+    fi;
+    gitdf checkout
+    gitdf config status.showUntrackedFiles no
 fi
+
+#cd "$XDG_CONFIG_HOME" && {
+#	git remote set-url origin $env_ssh
+#}
+
+# # install emacs
+# if [ ! -d "$XDG_CONFIG_HOME/emacs/.git" ]; then
+# 	# clone via HTTPS, as most likely SSH is not yet available or configured
+# 	git clone $env_emacs "$XDG_CONFIG_HOME"
+# fi
 
 #
 # Now start the Great Music
 #
 cd "$XDG_CONFIG_HOME" && {
-	./eru.sh install
+	./eru.sh # install
 }
