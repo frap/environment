10e89433b61d3c084326beedc04e8e78176f27fa
Gas <gas@tuatara.red>
2022-12-15 12:28:01 +1300

    updates to directories

--- eru.sh
+++ eru.sh
@@ -613,6 +613,7 @@ macos_guard && {
 	check brew || {
 	    log "Installing brew"
 	    /bin/bash -c  "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+	    eval "$(/opt/homebrew/bin/brew shellenv)"
 	    brew update
 	}
     }
