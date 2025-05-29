b7fe72f5080545e163080bab7d76acd98e89d39b
frap <1204361+frap@users.noreply.github.com>
2025-05-29 15:57:08 +1200

    massive rejig of essential - added prot packages and which-key

--- emacs/lisp/frap-essentials.el
+++ emacs/lisp/frap-essentials.el
@@ -9,6 +9,24 @@
 
 ;;; Code:
 
+(when (equal system-type 'darwin)
+  ;; Configure mac modifiers to be what I expect
+  (with-no-warnings
+    (setq  ns-command-modifier 'super
+           ns-option-modifier 'meta
+           ns-right-option-modifier 'nil
+           ns-right-command-modifier 'nil)))
+
+;; In Emacs for history reasons C-i is the same key as TAB. This is a problem inherited from terminal emulators. Using GUI we can do better
+;; Fix TAB and C-i (only in GUI)
+(defun setup-input (&rest _)
+  (when (display-graphic-p)
+    (define-key input-decode-map [(control ?i)] [control-i])
+    (define-key input-decode-map [(control ?I)] [(shift control-i)])))
+
+;; ;; If it's a daemon instance run setup-input each new frame
+(add-hook 'server-after-make-frame-hook 'setup-input)
+(add-hook 'after-init-hook 'setup-input)
 
 (defconst IS-MAC?     (eq system-type 'darwin))
 (defconst IS-LINUX?   (eq system-type 'gnu/linux))
@@ -46,6 +64,13 @@ unreadable. Returns the names of envvars that were changed."
             (unless (equal tz newtz)
               (set-time-zone-rule newtz))))
         env))))
+
+;; use-package is built-in as of Emacs 29, but since we use :bind, we
+;; need to load bind-key. If we forget, we get the error: Symbol's
+;; value as variable is void: personal-keybindings.
+(use-package bind-key
+  :ensure nil
+  :demand t)
 
 ;;; Essential configurations
 (use-package emacs
@@ -156,7 +181,7 @@ unreadable. Returns the names of envvars that were changed."
   ;; (add-hook 'prot-simple-file-to-register-jump-hook #'pulsar-reveal-entry))
   :bind
   ( :map prot-simple-override-mode-map
-    ("C-a" . prot-simple-duplicate-line-or-region) ; "again" mnemonic, overrides `move-beginning-of-line'
+    ;;    ("C-a" . prot-simple-duplicate-line-or-region) ; "again" mnemonic, overrides `move-beginning-of-line'
     ("C-d" . prot-simple-delete-line) ; overrides `delete-char'
 
     ("C-v" . prot-simple-multi-line-below) ; overrides `scroll-up-command'
@@ -211,6 +236,225 @@ unreadable. Returns the names of envvars that were changed."
     ;; Commands for files
     ("C-x r ." . prot-simple-file-to-register)))
 
+;;;; Region settings
+(use-package region-bindings
+  :ensure (:host gitlab :repo "andreyorst/region-bindings.el")
+  :preface
+  (defun region-bindings-off ()
+    (region-bindings-mode -1))
+  :hook
+  (after-init . global-region-bindings-mode)
+  (magit-mode . region-bindings-off)
+  ;; :init
+  ;; (add-hook 'region-bindings-mode-hook (lambda () (message "region-bindings-mode active")))
+  )
+
+(use-package expand-region
+  :ensure t
+  :bind (("M-2" . er/expand-region)
+         ("C-=" . er/expand-region)))
+
+(use-feature rect
+  :bind (("C-x r C-y" . rectangle-yank-add-lines))
+  :preface
+  (defun rectangle-yank-add-lines ()
+    (interactive "*")
+    (when (use-region-p)
+      (delete-region (region-beginning) (region-end)))
+    (save-restriction
+      (narrow-to-region (point) (point))
+      (yank-rectangle))))
+
+;;;; Multi Cursor
+
+;; This is globally useful, so it goes under `C-x', and `m'
+;; for "multiple-cursors" is easy to remember.
+;;(define-key ctl-x-map "\C-m" #'mc/mark-all-dwim)
+;; Usually, both `C-x C-m' and `C-x RET' invoke the
+;; `mule-keymap', but that's a waste of keys. Here we put it
+;; _just_ under `C-x RET'.
+;;(define-key ctl-x-map (kbd "<return>") mule-keymap)
+
+(use-package multiple-cursors
+  :ensure t
+  :preface
+  (defvar gas/mc-map (make-sparse-keymap))
+  (fset 'gas/mc-map gas/mc-map)
+  :commands (mc/edit-lines
+             mc/mark-all-like-this
+             mc/mark-next-like-this
+             mc/mark-previous-like-this)
+  :bind
+  (;; Remember `er/expand-region' is bound to M-2!
+   ("C->" . mc/mark-next-like-this)
+   ("M-3" . mc/mark-next-like-this)
+   ("M-#" . mc/unmark-next-like-this)
+   ("C-<" . mc/mark-previous-like-this)
+   ("M-4" . mc/mark-previous-like-this)
+   ("M-$" . mc/unmark-previous-like-this)
+
+   ("C-*" . mc/mark-all-like-this)
+   ("C-c m" . mc/mark-all-dwim)
+
+   ("C-M->" . mc/mark-next-symbol-like-this)
+   ("C-M-<" . mc/mark-previous-symbol-like-this)
+   ("C-M-*" . mc/mark-all-symbols-like-this)
+
+   :map region-bindings-mode-map
+   ("a" . mc/mark-all-symbols-like-this)
+   ("A" . mc/mark-all-like-this)
+   ("l" . mc/edit-ends-of-lines)
+   ("m" . mc/mark-all-dwim)
+   ("n" . mc/mark-next-symbol-like-this)
+   ("N" . mc/mark-next-like-this)
+   ("p" . mc/mark-previous-symbol-like-this)
+   ("P" . mc/mark-previous-like-this)
+   ("s" . mc/mark-all-in-region-regexp)
+   ;; ocassionaly useful
+   ("d" . mc/mark-all-symbols-like-this-in-defun)
+   ("t" . mc/reverse-regions)
+   ("i" . mc/insert-numbers)
+   ("h" . mc/hide-unmatched-lines-mode)
+   ("^" . mc/sort-regions)
+   ("v" . mc/vertical-align-with-space)
+   ("," . mc/unmark-next-like-this)
+   ("." . mc/skip-to-next-like-this)
+   ("C-a" . mc/edit-beginnings-of-lines)
+   ("C-e" . mc/edit-ends-of-lines)
+   ;; ("<down-mouse-1>" . mc/keyboard-quit)
+   ("<mouse-1>" . mc/keyboard-quit)
+   )
+  :config
+  (global-unset-key (kbd "M-<down-mouse-1>"))
+  (global-set-key (kbd "M-<mouse-1>") 'mc/add-cursor-on-click)
+
+  (with-eval-after-load 'multiple-cursors-core
+    ;; Immediately load mc list, otherwise it will show as
+    ;; changed as empty in my git repo
+    (mc/load-lists)))
+
+;;;; Editing parentheses
+
+(use-feature paren
+  :hook (prog-mode . show-paren-mode)
+  :config
+  ;;(show-paren-mode 1)
+  (setq show-paren-delay 0.1
+        show-paren-highlight-openparen t
+        show-paren-when-point-inside-paren t))
+
+(defun chee/puni-unwrap-sexp (&optional open close)
+  (interactive)
+  (save-excursion
+    (let* ((bounds (puni-bounds-of-sexp-around-point))
+           (beg (+ (car bounds) 1))
+           (end (- (cdr bounds) 1)))
+      (puni-kill-region beg end)
+      (puni-backward-delete-char)
+      (if open (insert-char open))
+      (yank)
+      (if close (insert-char close)))))
+
+(defun chee/puni-rewrap-sexp nil
+  (interactive)
+  (let ((open (read-char "Opening character? "))
+        (close (read-char "Closing character? ")))
+    (chee/puni-unwrap-sexp open close)))
+
+(use-package puni
+  :ensure (:host github :repo "AmaiKinono/puni")
+  :defer t
+  ;; :delight " ♾️"
+  :hook (((common-lisp-modes-mode nxml-mode) . puni-mode)
+         (puni-mode . electric-pair-local-mode))
+  :init
+  ; The autoloads of Puni are set up so you can enable `puni-mode` or
+  ;; `puni-global-mode` before `puni` is actually loaded. Only after you press
+  ;; any key that calls Puni commands, it's loaded.
+  (puni-global-mode)
+  (add-hook 'term-mode-hook #'puni-disable-puni-mode)
+  (add-hook 'eshell-mode-hook #'puni-disable-puni-mode)
+  ;; paredit-like keys
+  :bind
+  (("C-b"  . backword-word)
+   ("C-f"  . forward-word)
+   ("M-b"  . puni-backward-sexp-or-up-list)
+   ("M-f"  . puni-forward-sexp-or-up-list)
+   :map region-bindings-mode-map
+    ("(" . puni-wrap-round)
+    ("[" . puni-wrap-square)
+    ("{" . puni-wrap-curly)
+    ("<" . puni-wrap-angle)
+    ;; paredit-like keys
+    :map puni-mode-map
+    ;; ("C-=" . chee/puni-unwrap-sexp)
+    ;; ("C-." . chee/puni-rewrap-sexp)
+    ("C-M-f" . puni-forward-sexp-or-up-list)
+    ("C-M-b" . puni-backward-sexp-or-up-list)
+    ("C-M-t" . puni-transpose)
+    ;; slurping & barfing
+    ("C-<left>" . puni-barf-forward)
+    ("C-}" . puni-barf-forward)
+    ("C-<right>" . puni-slurp-forward)
+    ("C-)" . puni-slurp-forward)
+    ("C-(" . puni-slurp-backward)
+    ("C-M-<left>" . puni-slurp-backward)
+    ("C-{" . puni-barf-backward)
+    ("C-M-<right>" . puni-barf-backward)
+    ("C-(" . puni-slurp-backward)
+    ("M-(" . puni-barf-backward)
+    ("C-)" . puni-slurp-forward)
+    ("M-)" . puni-barf-forward)
+    ;; depth chaining
+    ("M-r" . puni-raise)
+    ("M-s" . puni-splice)
+    ;; ("M-<up>" . puni-splice-killing-backward)
+    ;; ("M-<down>" . puni-splice-killing-forward)
+    ("M-(" . puni-wrap-round)
+    ("M-{" . puni-wrap-curly)
+    ("M-?" . puni-convolute)
+    ("M-S" . puni-split)
+    ;; moving
+    ("M-<up>" . puni-beginning-of-sexp)
+    ("M-<down>" . puni-end-of-sexp))
+  :preface
+  (define-advice puni-kill-line (:before (&rest _) back-to-indentation)
+    "Go back to indentation before killing the line if it makes sense to."
+    (when (looking-back "^[[:space:]]*" nil)
+      (if (bound-and-true-p indent-line-function)
+          (funcall indent-line-function)
+        (back-to-indentation)))))
+
+(use-package puni
+  :when IS-GUI?
+  :ensure nil
+  :bind (:map puni-mode-map
+              ;; doesn't work in terminal
+              ("M-[" . puni-wrap-square)))
+
+;;;; Scratch buffers per major mode (prot-scratch.el)
+(use-package prot-scratch
+  :ensure nil
+  :bind ("C-c s" . prot-scratch-buffer)
+  :config
+  (setq prot-scratch-default-mode 'text-mode))
+
+;;;; Insert character pairs (prot-pair.el)
+(use-package prot-pair
+  :ensure nil
+  :bind
+  (("C-'" . prot-pair-insert)
+   ("M-'" . prot-pair-insert)
+   ("M-\\" . prot-pair-delete)))
+
+;;; comment-dwim-2
+;;; comment/un-comment
+(use-package comment-dwim-2
+  :ensure t
+  :bind ("M-;" . 'comment-dwim-2)
+  :delight)
+
+
 ;;;; Comments (prot-comment.el)
 (use-package prot-comment
   :ensure nil
@@ -229,11 +473,23 @@ unreadable. Returns the names of envvars that were changed."
    ("C-x C-;" . prot-comment-timestamp-keyword)))
 
 ;;;; Prefix keymap (prot-prefix.el)
+;; set of keymaps with commonly used commands and puts them behind a prefix map.
+;;Keymaps are organised thematically and rely on strong mnemonics, such as b for buffers, w for windows etc
 (use-package prot-prefix
   :ensure nil
   :bind-keymap
-  (("<insert>" . prot-prefix)
-   ("C-z" . prot-prefix)))
+  (("C-z" . prot-prefix)))
+
+;;; No littering
+;; Many packages leave crumbs in user-emacs-directory or even
+;; $HOME. Finding and configuring them individually is a hassle, so we
+;; rely on the community configuration of no-littering. Run this
+;; early, because many of the crumb droppers are configured below!
+(use-package no-littering
+  :ensure (:wait t)
+  :preface
+  (setq no-littering-etc-directory "~/.cache/emacs/etc/"
+	no-littering-var-directory "~/.cache/emacs/var/"))
 
 (use-package recentf
   :ensure nil
@@ -241,12 +497,21 @@ unreadable. Returns the names of envvars that were changed."
   :config
   (setq recentf-max-saved-items 100)
   (setq recentf-max-menu-items 25) ; I don't use the `menu-bar-mode', but this is good to know
-  (setq recentf-save-file-modes nil)
-  (setq recentf-keep nil)
-  (setq recentf-auto-cleanup nil)
-  (setq recentf-initialize-file-name-history nil)
-  (setq recentf-filename-handlers nil)
-  (setq recentf-show-file-shortcuts-flag nil))
+  ;; prot defaults below commented out as he doesnt use it
+  ;; (setq recentf-save-file-modes nil)
+  ;; (setq recentf-keep nil)
+  ;; (setq recentf-auto-cleanup nil)
+  ;; (setq recentf-initialize-file-name-history nil)
+  ;; (setq recentf-filename-handlers nil)
+  ;; (setq recentf-show-file-shortcuts-flag nil)
+  (add-to-list 'recentf-exclude "\\.gpg\\")
+  (dolist (dir (list (locate-user-emacs-file ".cache/")
+                     (locate-user-emacs-file "workspace/.cache/")))
+    (add-to-list 'recentf-exclude (concat (regexp-quote dir) ".*"))
+    (add-to-list 'recentf-exclude
+	         (recentf-expand-file-name no-littering-var-directory))
+    (add-to-list 'recentf-exclude
+	         (recentf-expand-file-name no-littering-etc-directory))))
 
 ;;;; Mouse and mouse wheel behaviour
 (use-package mouse
@@ -290,6 +555,10 @@ unreadable. Returns the names of envvars that were changed."
         set-mark-command-repeat-pop t))
 
 ;;;; Built-in bookmarking framework (bookmark.el)
+;; Bookmarks are compartments that store arbitrary information about a file or buffer.
+;; The records are used to recreate that file/buffer inside of Emacs.
+;; Use the bookmark-set command (C-x r m by default) to record a bookmark.
+;; visit one of your bookmarks with bookmark-jump (C-x r b by default).
 (use-package bookmark
   :ensure nil
   :commands (bookmark-set bookmark-jump bookmark-bmenu-list)
@@ -305,9 +574,10 @@ unreadable. Returns the names of envvars that were changed."
   (setq bookmark-save-flag 1))
 
 ;;;; Registers (register.el)
+;; registers are essential for keyboard macros.
 (use-package register
   :ensure nil
-  :defer t ; its commands are autoloaded, so this will be loaded then
+  :defer t  ; its commands are autoloaded, so this will be loaded then
   :config
   (setq register-preview-delay 0.8
         register-preview-function #'register-preview-default)
@@ -316,6 +586,7 @@ unreadable. Returns the names of envvars that were changed."
     (add-to-list 'savehist-additional-variables 'register-alist)))
 
 ;;;; Auto revert mode
+;; Update the contents of a saved buffer when its underlying file is changed externally. aka git pull
 (use-package autorevert
   :ensure nil
   :hook (after-init . global-auto-revert-mode)
@@ -323,6 +594,7 @@ unreadable. Returns the names of envvars that were changed."
   (setq auto-revert-verbose t))
 
 ;;;; Delete selection
+;; delete the selected text upon the insertion of new text
 (use-package delsel
   :ensure nil
   :hook (after-init . delete-selection-mode))
@@ -425,10 +697,155 @@ unreadable. Returns the names of envvars that were changed."
 (use-package server
   :ensure nil
   :defer 1
-  :commands (server-running-p)
   :config
-  (setq server-socket-dir (expand-file-name "~/.cache/emacs/server"))
+   (setq server-socket-dir (expand-file-name "~/.cache/emacs/server"))
+   (setq server-client-instructions nil)
   (unless (server-running-p)
     (server-start)))
+
+;;; Substitute
+;; Another package of mine... Video demo:
+;; <https://protesilaos.com/codelog/2023-01-16-emacs-substitute-package-demo/>.
+(use-package substitute
+  :ensure t
+  :defer 1
+  ;; Produce a message after the substitution that reports on what
+  ;; happened.  It is a single line, like "Substituted `TARGET' with
+  ;; `SUBSTITUTE' N times across the buffer.
+  :hook (substitute-post-replace . substitute-report-operation)
+  :commands
+  (substitute-target-below-point ; Forward motion like isearch (C-s)
+   substitute-target-above-point ; Backward motion like isearch (C-r)
+   substitute-target-in-defun    ; inside of the current definition
+   substitute-target-in-buffer)  ; throughout the buffer
+  :config
+  ;; Set this to non-nil to highlight all occurrences of the current
+  ;; target.
+  (setopt substitute-highlight t)
+
+  ;; Set this to t if you want to always treat the letter casing
+  ;; literally.  Otherwise each command accepts a `C-u' prefix
+  ;; argument to do this on-demand.
+  (setq substitute-fixed-letter-case nil)
+
+  ;; C-c s is occupied by `prot-scratch-buffer'.
+  (define-key global-map (kbd "C-c r") #'substitute-prefix-map))
+
+;; moves the cursor to the point where the last change happened
+(use-package goto-chg
+  :ensure t
+  :bind
+  (("C-(" . goto-last-change)
+   ("C-)" . goto-last-change-reverse)))
+
+;;; Mark syntactic constructs efficiently if tree-sitter is available (expreg)
+(when (treesit-available-p)
+  (use-package expreg
+    :ensure t
+    :functions (prot/expreg-expand prot/expreg-expand-dwim)
+    ;; There is also an `expreg-contract' command, though I have no use for it.
+    :bind ("C-M-SPC" . prot/expreg-expand-dwim) ; overrides `mark-sexp'
+    :config
+    (defun prot/expreg-expand (n)
+      "Expand to N syntactic units, defaulting to 1 if none is provided interactively."
+      (interactive "p")
+      (dotimes (_ n)
+	(expreg-expand)))
+
+    (defun prot/expreg-expand-dwim ()
+      "Do-What-I-Mean `expreg-expand' to start with symbol or word.
+If over a real symbol, mark that directly, else start with a
+word.  Fall back to regular `expreg-expand'."
+      (interactive)
+      (let ((symbol (bounds-of-thing-at-point 'symbol)))
+	(cond
+	 ((equal (bounds-of-thing-at-point 'word) symbol)
+	  (prot/expreg-expand 1))
+	 (symbol (prot/expreg-expand 2))
+	 (t (expreg-expand)))))))
+
+;;; Pass interface (password-store)
+(use-package password-store
+  :ensure t
+  ;; Mnemonic is the root of the "code" word (κώδικας).  But also to add
+  ;; the password to the kill-ring.  Other options are already taken.
+  :bind ("C-c k" . password-store-copy)
+  :config
+  (setq password-store-time-before-clipboard-restore 30))
+
+(use-package pass
+ :ensure t
+ :commands (pass))
+
+;;; Shell (M-x shell)
+(use-package shell
+  :ensure nil
+  :bind
+  ( :map shell-mode-map
+    ("C-c C-k" . comint-clear-buffer)
+    ("C-c C-w" . comint-write-output))
+  :config
+  ;; Check my .bashrc which handles `comint-terminfo-terminal':
+  ;;
+  ;; # Default pager.  The check for the terminal is useful for Emacs with
+  ;; # M-x shell (which is how I usually interact with bash these days).
+  ;; #
+  ;; # The COLORTERM is documented in (info "(emacs) General Variables").
+  ;; # I found the reference to `dumb-emacs-ansi' in (info "(emacs)
+  ;; # Connection Variables").
+  ;; if [ "$TERM" = "dumb" ] && [ "$INSIDE_EMACS" ] || [ "$TERM" = "dumb-emacs-ansi" ] && [ "$INSIDE_EMACS" ]
+  ;; then
+  ;;     export PAGER="cat"
+  ;;     alias less="cat"
+  ;;     export TERM=dumb-emacs-ansi
+  ;;     export COLORTERM=1
+  ;; else
+  ;;     # Quit once you try to scroll past the end of the file.
+  ;;     export PAGER="less --quit-at-eof"
+  ;; fi
+
+  (setq shell-command-prompt-show-cwd t) ; Emacs 27.1
+  (setq ansi-color-for-comint-mode t)
+  (setq shell-input-autoexpand 'input)
+  (setq shell-highlight-undef-enable t) ; Emacs 29.1
+  (setq shell-has-auto-cd nil) ; Emacs 29.1
+  (setq shell-get-old-input-include-continuation-lines t) ; Emacs 30.1
+  (setq shell-kill-buffer-on-exit t) ; Emacs 29.1
+  (setq shell-completion-fignore '("~" "#" "%"))
+  (setq-default comint-scroll-to-bottom-on-input t)
+  (setq-default comint-scroll-to-bottom-on-output nil)
+  (setq-default comint-input-autoexpand 'input)
+  (setq comint-prompt-read-only t)
+  (setq comint-buffer-maximum-size 9999)
+  (setq comint-completion-autolist t)
+  (setq comint-input-ignoredups t)
+  (setq tramp-default-remote-shell "/bin/bash")
+
+  (setq shell-font-lock-keywords
+        '(("[ \t]\\([+-][^ \t\n]+\\)" 1 font-lock-builtin-face)
+          ("^[^ \t\n]+:.*" . font-lock-string-face)
+          ("^\\[[1-9][0-9]*\\]" . font-lock-constant-face)))
+
+  ;; Support for OS-specific escape sequences such as what `ls
+  ;; --hyperlink' uses.  I normally don't use those, but I am checking
+  ;; this to see if there are any obvious advantages/disadvantages.
+  (add-hook 'comint-output-filter-functions 'comint-osc-process-output))
+
+(use-package prot-shell
+  :ensure nil
+  :bind (("<f1>" . prot-shell)) ; I don't use F1 for help commands
+  :hook (shell-mode . prot-shell-mode))
+
+(use-package which-key
+  :ensure nil ; built into Emacs 30
+  :hook (after-init . which-key-mode)
+  :config
+  (setq which-key-separator "  ")
+  (setq which-key-prefix-prefix "... ")
+  (setq which-key-max-display-columns 3)
+  (setq which-key-idle-delay 0.3)
+  (setq which-key-idle-secondary-delay 0.25)
+  (setq which-key-add-column-padding 1)
+  (setq which-key-max-description-length 40))
 
 (provide 'frap-essentials)
