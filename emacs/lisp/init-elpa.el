0089542e8cc2a2e362757c0375328b8999ac22d4
frap <1204361+frap@users.noreply.github.com>
2025-12-06 18:39:17 +1300

    add prot-emacs-install macro

--- emacs/lisp/init-elpa.el
+++ emacs/lisp/init-elpa.el
@@ -21,22 +21,81 @@
 ;; package.el setup
 ;; ----------------------------------------------------------
 
-(require 'package)
+(setq package-vc-register-as-project nil) ; Emacs 30
+
+(add-hook 'package-menu-mode-hook #'hl-line-mode)
 
 (setq package-archives
       '(("gnu"   . "https://elpa.gnu.org/packages/")
         ("melpa" . "https://melpa.org/packages/")))
 
-;; We disabled automatic package loading in early-init in the past.
-;; Now we control it manually here.
-(setq package-enable-at-startup t)
+;; Also read: <https://protesilaos.com/codelog/2022-05-13-emacs-elpa-devel/>
+(setq package-archives
+      '(("gnu-elpa" . "https://elpa.gnu.org/packages/")
+        ("gnu-elpa-devel" . "https://elpa.gnu.org/devel/")
+        ("nongnu" . "https://elpa.nongnu.org/nongnu/")
+        ("melpa" . "https://melpa.org/packages/")))
 
-(unless package--initialized
-  (package-initialize))
+;; Highest number gets priority (what is not mentioned has priority 0)
+(setq package-archive-priorities
+      '(("gnu-elpa" . 3)
+        ("melpa" . 2)
+        ("nongnu" . 1)))
 
-(unless package-archive-contents
-  (package-refresh-contents))
 
+defvar prot-emacs-my-packages
+'(beframe
+  doric-themes
+  ef-themes
+  fontaine
+  ;; lin
+  logos
+  modus-themes
+  pulsar
+  show-font
+  ;; spacious-padding
+  standard-themes
+  ;; substitute
+  theme-buffet)
+"List of symbols representing prots packges that I want latest.")
+
+(setq package-pinned-packages
+      `(,@(mapcar
+           (lambda (package)
+             (cons package "gnu-elpa-devel"))
+           prot-emacs-my-packages)))
+
+(defmacro prot-emacs-install (package &rest vc-args)
+  "Prepare to install PACKAGE.
+PACKAGE is an unquoted symbol, referring to the name of the package.  If
+VC-ARGS are nil, then install PACKAGE using `package-install'.
+
+If VC-ARGS is non-nil, then check if their `car' is a directory.  If it
+is, apply `package-vc-install-from-checkout' on VC-ARGS, else apply
+`package-vc-install'.
+
+At all times, do nothing if PACKAGE is already installled."
+  (declare (indent 0))
+  (unless (symbolp package)
+    (error "The package `%s' is not a symbol" package))
+  (cond
+   ((and package vc-args)
+    (let ((fn (if-let* ((first (car vc-args))
+                        (_ (and (stringp first) (file-directory-p first))))
+                  'package-vc-install-from-checkout
+                'package-vc-install)))
+      `(unless (package-installed-p ',package)
+         (condition-case-unless-debug err
+             (apply #',fn ,vc-args)
+           (error (message "Failed `%s' with `%S': `%S'" ',fn ,vc-args (cdr err)))))))
+   (package
+    `(progn
+       (unless (package-installed-p ',package)
+         (unless package-archive-contents
+           (package-refresh-contents))
+         (condition-case-unless-debug nil
+             (package-install ',package)
+           (error (message "Cannot install `%s'; try `M-x package-refresh-contents' first" ',package))))))))
 ;; ----------------------------------------------------------
 ;; use-package: prefer built-in in Emacs 29+
 ;; ----------------------------------------------------------
