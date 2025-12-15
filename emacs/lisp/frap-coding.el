afa1aa472e390c6230da24b3a8578f4f1142eda3
frap <1204361+frap@users.noreply.github.com>
2025-12-16 11:41:31 +1300

    rm additional treesit

--- emacs/lisp/frap-coding.el
+++ emacs/lisp/frap-coding.el
@@ -358,22 +358,10 @@ Uses puni if present and active."
 ;;   (global-treesit-auto-mode 1)))
 
 
-(use-package treesit
-  :ensure nil
-  :mode (("\\.clj\\'"  . clojure-ts-mode)
-         ("\\.edn\\'"  . clojure-ts-mode)
-         ("\\.cljs\\'"  . clojure-ts-mode)
-         ("\\.tsx\\'" . tsx-ts-mode)
-         ("\\.js\\'"  . typescript-ts-mode)
-         ("\\.mjs\\'" . typescript-ts-mode)
-         ("\\.mts\\'" . typescript-ts-mode)
-         ("\\.cjs\\'" . typescript-ts-mode)
-         ("\\.ts\\'"  . typescript-ts-mode)
-         ("\\.jsx\\'" . tsx-ts-mode)
-         ("\\.json\\'" .  json-ts-mode)
+(use-feature treesit
+  :mode (
          ("\\.Dockerfile\\'" . dockerfile-ts-mode)
          ("\\.prisma\\'" . prisma-ts-mode)
-         ;; More modes defined here...
          )
   :preface
   (defun os/setup-install-grammars ()
@@ -475,108 +463,6 @@ Uses puni if present and active."
 (use-feature subword
   :defer t
   :delight)
-
-;; (eval-and-compile
-;;   ;; Define it so “void-variable” can’t happen if treesit isn't loaded yet.
-;;   (defvar treesit-language-source-alist nil))
-;;
-;; (with-eval-after-load 'treesit
-;;       (add-to-list 'treesit-language-source-alist
-;;              '(clojure "https://github.com/sogaiu/tree-sitter-clojure")))
-;; ;;; Mark syntactic constructs efficiently if tree-sitter is available (expreg)
-;; (when (treesit-available-p)
-;;   (use-package treesit-auto
-;;   :ensure t
-;;   :custom
-;;   (treesit-auto-install 'prompt) ;; auto install missing grammars with prompt
-;;   :config
-;;   (setq major-mode-remap-alist
-;;       '((clojure-mode . clojure-ts-mode)
-;;         (clojurescript-mode . clojure-ts-mode)
-;;         (clojurec-mode . clojure-ts-mode)))
-;;   (setq treesit-font-lock-level 4)
-;;   (treesit-auto-add-to-auto-mode-alist 'all) ;; all known remappings
-;;   (global-treesit-auto-mode 1)))
-
-
-(use-package treesit
-  :ensure nil
-  :mode (("\\.clj\\'"  . clojure-ts-mode)
-         ("\\.edn\\'"  . clojure-ts-mode)
-         ("\\.cljs\\'"  . clojure-ts-mode)
-         ("\\.tsx\\'" . tsx-ts-mode)
-         ("\\.js\\'"  . typescript-ts-mode)
-         ("\\.mjs\\'" . typescript-ts-mode)
-         ("\\.mts\\'" . typescript-ts-mode)
-         ("\\.cjs\\'" . typescript-ts-mode)
-         ("\\.ts\\'"  . typescript-ts-mode)
-         ("\\.jsx\\'" . tsx-ts-mode)
-         ("\\.json\\'" .  json-ts-mode)
-         ("\\.Dockerfile\\'" . dockerfile-ts-mode)
-         ("\\.prisma\\'" . prisma-ts-mode)
-         ;; More modes defined here...
-         )
-  :preface
-  (defun os/setup-install-grammars ()
-    "Install Tree-sitter grammars if they are absent."
-    (interactive)
-    (dolist (grammar
-             '((clojure "https://github.com/sogaiu/tree-sitter-clojure")
-               (css . ("https://github.com/tree-sitter/tree-sitter-css" "v0.20.0"))
-               (bash "https://github.com/tree-sitter/tree-sitter-bash")
-               (html . ("https://github.com/tree-sitter/tree-sitter-html" "v0.20.1"))
-               (javascript . ("https://github.com/tree-sitter/tree-sitter-javascript" "v0.21.2" "src"))
-               (json . ("https://github.com/tree-sitter/tree-sitter-json" "v0.20.2"))
-               (python . ("https://github.com/tree-sitter/tree-sitter-python" "v0.20.4"))
-               (go "https://github.com/tree-sitter/tree-sitter-go" "v0.20.0")
-               (markdown "https://github.com/ikatyang/tree-sitter-markdown")
-               (make "https://github.com/alemuller/tree-sitter-make")
-               (elisp "https://github.com/Wilfred/tree-sitter-elisp")
-               (cmake "https://github.com/uyha/tree-sitter-cmake")
-               (c "https://github.com/tree-sitter/tree-sitter-c")
-               (cpp "https://github.com/tree-sitter/tree-sitter-cpp")
-               (toml "https://github.com/tree-sitter/tree-sitter-toml")
-               (tsx . ("https://github.com/tree-sitter/tree-sitter-typescript" "v0.20.3" "tsx/src"))
-               (typescript . ("https://github.com/tree-sitter/tree-sitter-typescript" "v0.20.3" "typescript/src"))
-               (yaml . ("https://github.com/ikatyang/tree-sitter-yaml" "v0.5.0"))
-               (prisma "https://github.com/victorhqc/tree-sitter-prisma")
-               (terraform "https://github.com/tree-sitter-grammars/tree-sitter-hcl")
-               ))
-      (add-to-list 'treesit-language-source-alist grammar)
-      ;; Only install `grammar' if we don't already have it
-      ;; installed. However, if you want to *update* a grammar then
-      ;; this obviously prevents that from happening.
-      (unless (treesit-language-available-p (car grammar))
-        (treesit-install-language-grammar (car grammar)))))
-
-  ;; Optional, but recommended. Tree-sitter enabled major modes are
-  ;; distinct from their ordinary counterparts.
-  ;;
-  ;; You can remap major modes with `major-mode-remap-alist'. Note
-  ;; that this does *not* extend to hooks! Make sure you migrate them
-  ;; also
-  (dolist (mapping
-           '((clojure-mode . clojure-ts-mode)
-             (clojurescript-mode . clojure-ts-mode)
-             (clojurec-mode . clojure-ts-mode)
-             (python-mode . python-ts-mode)
-             (css-mode . css-ts-mode)
-             (typescript-mode . typescript-ts-mode)
-             (js-mode . typescript-ts-mode)
-             (js2-mode . typescript-ts-mode)
-             (c-mode . c-ts-mode)
-             (c++-mode . c++-ts-mode)
-             (c-or-c++-mode . c-or-c++-ts-mode)
-             (bash-mode . bash-ts-mode)
-             (css-mode . css-ts-mode)
-             (json-mode . json-ts-mode)
-             (js-json-mode . json-ts-mode)
-             (sh-mode . bash-ts-mode)
-             (sh-base-mode . bash-ts-mode)
-             ))
-    (add-to-list 'major-mode-remap-alist mapping))
-  :config
-  (os/setup-install-grammars))
 
 ;;;; CSS Web
 (use-feature css-mode
