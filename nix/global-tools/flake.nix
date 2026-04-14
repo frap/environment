fb59255b9225add971fda0469b4af6a6de932b03
frap <1204361+frap@users.noreply.github.com>
2026-04-14 12:26:29 +1200

    update nix global-tools

--- nix/global-tools/flake.nix
+++ nix/global-tools/flake.nix
@@ -10,26 +10,45 @@
       pkgs = import nixpkgs { inherit system; };
     in
     {
-      packages.${system}.default = pkgs.buildEnv {
-        name = "global-tools";
-        paths = [
-          pkgs.awscli2
-          pkgs.babashka
-          pkgs.bashInteractive
-          pkgs.bat
-          pkgs.clojure
-          pkgs.devenv
-          pkgs.direnv
-          pkgs.eza
-          pkgs.fd
-          pkgs.gh
-          pkgs.git
-          pkgs.gnupg
-          pkgs.jet
-          pkgs.jq
-          pkgs.ripgrep
-          pkgs.rsync      
-        ];
+      packages.${system} = {
+        default = pkgs.buildEnv {
+          name = "global-tools";
+          paths = with pkgs; [
+            awscli2
+            babashka
+            bashInteractive
+            bat
+            clojure
+            devenv
+            direnv
+            eza
+            fd
+            gh
+            git
+            gnupg
+            jet
+            jq
+            ripgrep
+            rsync      
+          ];
       };
-    };
+
+      awscli2 = pkgs.awscli2;
+      babashka = pkgs.babashka;
+      bashInteractive = pkgs.bashInteractive;
+      bat = pkgs.bat;
+      clojure = pkgs.clojure;
+      devenv =   pkgs.devenv;
+      direnv =    pkgs.direnv;
+      eza = pkgs.eza;
+      fd = pkgs.fd;
+      gh = pkgs.gh;
+      git = pkgs.git;
+      gnupg = pkgs.gnupg;
+      jet = pkgs.jet;
+      jq = pkgs.jq;
+      ripgrep = pkgs.ripgrep;
+      rsync = pkgs.rsync; 
+    };   
+  };
 }
